d9539fcc96e165cbf7f82fc5f97dac905e53fabe
Aslam Hossain <aslam.ece.hstu@gmail.com>
2024-11-07 20:32:16 +0600

    Updated UI

--- src/main/java/com/practice/chatbot/dto/User.java
+++ src/main/java/com/practice/chatbot/dto/User.java
@@ -11,7 +11,7 @@ import lombok.Data;
 @Data
 @Builder
 public class User {
-    private String id;
+    private String username;
     private String message;
     private MessageType messageType;
 }
